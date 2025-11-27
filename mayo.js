fb3af0c019c09e5f1668bce1ce862928f6e36721
Cinthya Agüero <cagueroc@ccss.sa.cr>
2025-11-27 14:48:36 -0600

    re resuelve conflicto

--- mayo.js
+++ mayo.js
@@ -3,4 +3,15 @@ console.log("mayo");
 const dias = 30;
 const mes = "Junio";
 const num_mes =6;
-let semestre =(num_mes<=6)?"primer trimestre":"segundo";
+let semestre ="";
+
+if(num_mes <=6)
+{
+  semestre = "primer semestre";
+}
+else
+{
+  semestre ="segundo semestre";
+}
+
+console.log(semestre);
