cca3b7ce7e4fa96b3d5bf5a321ab615dce42372e
cagueroc <cagueroc@ccss.sa.cr>
2025-11-27 14:41:03 -0600

    feat : se crea mayo desde githut

---
+++ mayo.js
@@ -0,0 +1,17 @@
+console.log("mayo");
+
+const dias = 30;
+const mes = "Junio";
+const num_mes =6;
+let semestre ="";
+
+if(num_mes <=6)
+{
+  semestre = "primer semestre";
+}
+else
+{
+  semestre ="segundo semestre";
+}
+
+console.log(semestre);
